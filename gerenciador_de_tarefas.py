ffd233a1885d9d381a9556deea14ee41d5d79774
Rodolfo Almeida <rodolfogaspardealmeida@gmail.com>
2026-07-04 15:30:37 -0300

    Mais um dia de estudos e pratica

--- gerenciador_de_tarefas.py
+++ gerenciador_de_tarefas.py
@@ -10,5 +10,36 @@ def carregar_tarefas():
         with open('tarefas.json', 'r') as arquivo:
             return json.load(arquivo)
     return []    
-    
 
+# listar as tarefas
+def listar_tarefas(tarefas):
+    print("Tarefas:")
+    for tarefa in tarefas:
+        print(f"ID: {tarefa['id']}, Titulo: {tarefa['titulo']}")
+    
+# Menu de Tarefas
+
+def menu():
+    print("=== GRENCIADOR DE TAREFAS ===")
+    print("1. Adicionar Tarefa")
+    print("2. Listar Tarefas")
+    print("3. concluir Tarefa")
+    print("4. Remover Tarefa")
+    print("5. Sair")
+    return input("Escolha uma opção: ")
+
+# função central do meu sistema que sera um loop das ações 
+
+def main():
+    tarefas = carregar_tarefas()
+    while True:
+        opcao = menu()
+        if opcao == '1':
+            break
+        if opcao == '2':
+            listar_tarefas(tarefas)
+
+
+
+if __name__ == '__main__':
+    main()
